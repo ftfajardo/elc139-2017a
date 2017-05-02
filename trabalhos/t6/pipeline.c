618f51f81c126b1fe04965cb4f663e4a46e1c2c8
ftfajardo <ftfajardo@gmail.com>
2017-05-02 02:20:17 -0300

    Update pipeline.c

--- trabalhos/t6/pipeline.c
+++ trabalhos/t6/pipeline.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    int ntasks, rank, msgin = 0,msgout;
+    int ntasks, rank, msgin = 0;
     MPI_Status status;
 
     MPI_Init(&argc,&argv);
@@ -16,16 +16,16 @@ int main(int argc, char **argv) {
     }
     //outros processos	
     else if (rank < ntasks-1) {
-            MPI_Recv(&msgout, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            printf("ID %d recebe %d \n", rank, msgout);
-            msgin = msgout+1;// somei soh aqui nao sei se era pra somar antes e depois
+            MPI_Recv(&msgin, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("ID %d recebe %d \n", rank, msgin);
+            msgin++;// somei soh aqui nao sei se era pra somar antes e depois
             MPI_Send(&msgin, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
             printf("ID %d envia %d \n", rank, msgin);
     }	
 	//final	
     else {     
-        MPI_Recv(&msgout, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);       
-	printf("ID %d recebe %d\n", rank, msgout);
+        MPI_Recv(&msgin, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);       
+	printf("ID %d recebe %d\n", rank, msgin);
     }
 
     MPI_Finalize();

aa2324a72a5b5b6bac98d3112e7039b5a7fb6025
ftfajardo <ftfajardo@gmail.com>
2017-06-09 20:21:58 -0300

    Create sequencialcrack.c

--- trabalhos/t8/sequencialcrack.c
+++ trabalhos/t8/sequencialcrack.c
@@ -9,21 +9,25 @@ char text[] = "crackme.txt";
 
 //conta linhas
 int conta_linha(){
-	FILE *fp = fopen(text,"r");
-	if(fp == NULL) {
-  	printf("Erro ao abrir arquivo\n");
-		exit(1); 
-  }
-	int lines,ch;
-	while(!feof(fp)){
-		ch = fgetc(fp);
-		if(ch == '\n'){
-			lines++;
-		}
-	}  
-  fclose(fp);
-	return lines;
-}
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 33;
+	ssize_t nread;
+	int cont = 0;
+	stream = fopen(text, "r");
+	if (stream == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((nread = getline(&line, &len, stream)) != -1) {
+		cont++;
+	}
+	//printf("%d",cont);
+	free(line);
+	fclose(stream);
+	return cont;
+	}
 
 
 //preenche matriz
@@ -31,7 +35,7 @@ char** p_matriz(char **a){
 	FILE *fp = fopen(text,"r");
 	if(fp == NULL) {
 		printf("Erro ao abrir arquivo\n");
-		exit(1); 
+		exit(1);
   }
 	int i=0,j=0,ch;
 	do {
@@ -39,8 +43,8 @@ char** p_matriz(char **a){
 		if( feof(fp) ) {
     	break ;
     }
-		printf("%c", ch);
-		if(ch != '\n'){			
+		//printf("%c", ch);
+		if(ch != '\n'){
 			a[i][j] = ch;
 			j++;
 		}
@@ -48,7 +52,7 @@ char** p_matriz(char **a){
 			a[i][j]= '\0';
 			i++;
 			j = 0;
-		}   	 
+		}
 	}while(1);
 	fclose(fp);
 	return a;
@@ -64,35 +68,66 @@ long wtime()
 
 //printa o arquivo na tela
 void get_popen(char comand[]){
-   
+
 	FILE *fp;
 	int status;
-	char path[PATH_MAX];
-	
+	int cont = 0,i,cont2 =0,anterior = 0;
+	int c,entrei = 0,acabou = 0;	
+	char *resp = (char*)malloc(sizeof(char)*COL); 	
 	fp = popen(comand, "r");
 	if (fp == NULL)
 		exit(1);
 	
-	while (fgets(path, PATH_MAX, fp) != NULL)
-		printf("%s", path);
-    
+	while ((c = fgetc (fp)) != EOF){
+		if(c == '='){//ve se o char eh =
+			anterior = 1;
+		}
+		if(anterior == 1 && c == '>'){// se o anterior for = e o atual > coloca um marcador
+			cont = 1;
+		}
+		if(c != '='){//se o char for difente o anterior fica 0
+			anterior = 0;
+		}
+		if(entrei == 1 && (c == '\n' || c == '\0')){//entrei comeca em 0 logo esse if eh para quando ja achou a senha e precisa colocar o /0
+			*(resp+cont2) = '\0';
+			acabou = 1;
+		}
+
+		if(cont == 1 && cont2 <= 31 && c !='\n' && c != ' ' && acabou == 0){//marcador em 1 logo passou do => e aqui entra na seta > depois na 1 letra, marca o entrei
+			*(resp+cont2) = c;
+			cont2++;
+			entrei = 1;
+		}
+		if(cont2 == 32 && acabou == 0){//caso estoure o limite de 32 na senha só é colocado o /0 e avisado
+			*(resp+cont2) = '\0';printf("senha muito grande");
+		}
+		//putchar (c);
+	}
+	//print da senha comentado
+	fprintf(stderr,"Senha crackeada: ");
+	for( i = 0; i < COL; ++i){
+    fprintf(stderr,"%c", *(resp+i));
+	}
+  fprintf(stderr,"\n");
 	status = pclose(fp);
   if (status == -1) {
-  	exit(1);     
-  } 
+  	exit(1);
+  }
 	else {
         /* Use macros described under wait() to inspect `status' in order
            to determine success/failure of command executed by popen() */
-        
+
   }
+	free(resp);
+	resp = NULL;
 }
 
 
 //imprime a matriz
 void imprime(char **a,int lines){
-	int i ,j;	
+	int i ,j;
 	for(i = 0 ; i < lines ; i++){
-		for(j = 0 ; j < COL; j++){			
+		for(j = 0 ; j < COL; j++){
 			printf("%c",a[i][j]);
 		}
 		printf("\n");
@@ -101,18 +136,18 @@ void imprime(char **a,int lines){
 
 //libera a matriz
 char** libera(char **mat,int lines){
-	int i;	
-	printf("liberando \n");
+	int i;
+	printf("\nliberando \n");
 	for (i=0; i<lines; i++) free (mat[i]); /* libera as linhas da matriz */
-  free (mat);   
+  free (mat);
 	return NULL;
 }
 //aloca a matriz
-char** malloca(char **a, int lines){
-	a = (char **) malloc(sizeof(char*) * lines);
-	if (a == NULL ) {		
+char** malloca( int lines){
+	char **a = (char **) malloc(sizeof(char*) * lines);
+	if (a == NULL ) {
 		printf("Erro de alocacao de memoria\n");
-		exit(1);   
+		exit(1);
   }
 	int i;
 	for(i = 0 ; i < lines ; i++){
@@ -126,41 +161,40 @@ char** malloca(char **a, int lines){
 }
 
 int main(int argc, char* argv[]){
-	int n;
 	long start_time, end_time;
-	int ch=0;
-	int lines=0;	
+	int lines=0;
 	char **a;
-	int i,j;
+	int i;
 
 	//Conta linhas
 	lines = conta_linha();
-	
-	//Maloca vetor 
-	a = malloca(a,lines);
-	printf("%d \n",lines);
-	
+
+	//Maloca vetor
+	a = malloca(lines);
+	printf("linhas: (%d) \n",lines);
+
 	//Preenche matriz
 	a = p_matriz(a);
 
 	//Imprime matriz
-	//imprime(a,lines);  
+	//imprime(a,lines);
 
-		
-	char Str[80];
+
+	char Str[150];
 
 	start_time = wtime();
 	for(i = 0 ; i < lines ; i++){
 		strcpy (Str,"./mdcrack -M MD5 -s abcdefghijklmnopqrstuvwxyz ");
 		strcat (Str,a[i]);
 		puts (Str);
+
 		get_popen(Str);
 	}
 	end_time = wtime();
 	printf("Tempo: %ld", (long) (end_time - start_time));
 
 	a	=	libera(a,lines);
-		
+
 
 
 return 0;
